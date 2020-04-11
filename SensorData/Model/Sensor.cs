f9fd82e46ef2b38ee310a27749462b837cf6ddb1
DarrenWainwright <DWainwright@buckland.com>
2020-04-11 19:33:06 -0400

    Added a GetSensors endpoint

---
+++ SensorData/Model/Sensor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SensorData.Model
+{
+    public class Sensor
+    {
+        public Sensor(string id, string name, string type, int hearbeatInterval, DateTime lastHeartbeat)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            HearbeatInterval = hearbeatInterval;
+            LastHeartbeat = lastHeartbeat;
+        }
+
+        public Sensor(string id, string name, string type, int hearbeatInterval, string jsonLastHeartbeat)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            HearbeatInterval = hearbeatInterval;
+            LastHeartbeat = DateTime.Parse(jsonLastHeartbeat);
+        }
+
+
+        public string Id { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public int HearbeatInterval { get; }
+        public DateTime LastHeartbeat { get; }
+    }
+}
---
+++ SensorData/Query/GetSensors.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SensorData.Query
+{
+    public class GetSensors
+    {
+        private readonly ILogger<GetSensors> _logger;
+        private readonly Container _container;
+
+        public GetSensors(CosmosClient cosmosClient, ILogger<GetSensors> logger)
+        {
+            _container = cosmosClient.GetDatabase("MotherCluckers").GetContainer("Sensor");
+            _logger = logger;
+        }
+
+
+        [FunctionName(nameof(GetSensors))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+        {
+            Common.Log(_logger, $"Function: {nameof(GetSensors)} started with a HTTP Trigger");
+
+            try
+            {
+                // At this time, just return all sensors data
+                var iterator = _container.GetItemLinqQueryable<dynamic>().ToFeedIterator();
+
+                List<Model.Sensor> results = new List<Model.Sensor>();
+                //Asynchronous query execution
+                while (iterator.HasMoreResults)
+                    foreach (var item in await iterator.ReadNextAsync())
+                        results.Add(new Model.Sensor((string)item.id, (string)item.name, (string)item.type, (int)item.heartbeatInterval, (string)item.lastConnected));
+
+                return new OkObjectResult(results);
+            }
+            catch (System.Exception ex)
+            {
+
+                return new BadRequestObjectResult(ex.Message);
+            }
+
+
+        }
+    }
+}
